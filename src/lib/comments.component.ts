63221229b6017347977b90d2f97b812248d4e853
Dequevauviller Benjamin <ben@passiverecords.com>
2018-05-07 12:33:58 +0200

    init comments

--- src/lib/comments.component.ts
+++ src/lib/comments.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-comments',
-  template: `
-    <p>
-      comments works!
-    </p>
-  `,
+  selector: '[lib-comments]',
+  templateUrl: './comments.component.html',
   styles: []
 })
 export class CommentsComponent implements OnInit {
