95cf88d2bbcabb17f31ac23b423570ffdb4e4edb
Dequevauviller Benjamin <ben@passiverecords.com>
2018-05-07 15:59:06 +0200

    first commit :)

--- src/lib/comments.module.ts
+++ src/lib/comments.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommentsComponent } from './comments.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { CommentsComponent } from "./comments.component";
+import { MomentModule } from "ngx-moment";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [CommentsComponent],
-  exports: [CommentsComponent]
+    imports: [CommonModule, MomentModule, FormsModule],
+    declarations: [CommentsComponent],
+    exports: [CommentsComponent]
 })
-export class CommentsModule { }
+export class CommentsModule {}
